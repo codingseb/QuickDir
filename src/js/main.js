39e02d64f54cf467e4697312e34b056c5f5391d7
Coding Seb <sebgeiser@gmail.com>
2016-08-15 14:35:47 +0200

    open the current directory on F5

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 let autocompletion = require('./js/dir-autocomplete.js');
 let dirutils = require('./js/dir-utils.js');
+let shell = require('shelljs');
+let isWin = /^win/.test(process.platform);
+let runCmd = isWin ? 'explorer ' : 'open ';
+let field = null;
 
 dirutils.init(function(){
     $(function() {
         let body = $("body");
-        let field = $(".autocomplete");
+        field = $(".autocomplete");
 
         // Set the auto-complete to the field
         field.autocomplete({
@@ -25,13 +29,30 @@ dirutils.init(function(){
         });
 
         // Clear field on Escape key
-        field.keydown(function (e) {
-            if(e.which === 27){
-                field.val('');
-            }
-        });
+        field.keydown(onkeydown);
 
         // Focus the field as soon as the app is on
         field.focus();
     });
 });
+
+function onkeydown(e){
+    if(e.which === 27){
+        field.val('');
+        field.autocomplete("close");
+    }
+    else if(e.which === 116){
+        showcurrentdirectory();
+    }
+}
+
+function showcurrentdirectory(){
+    try{
+        let cmd = runCmd + '"' + field.val() + '"';
+        console.log(cmd);
+        shell.exec(cmd, function(code, stdout, stderr){
+            console.log({code, stdout, stderr});
+        });
+    }
+    catch(ex) {}
+}
