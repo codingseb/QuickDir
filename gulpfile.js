17a125a54b09f2ed5c7668dee5ad993f02b8a34e
Coding Seb <sebgeiser@gmail.com>
2016-08-19 07:23:05 +0200

    Remove blank lines

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var childProcess = require('child_process');
 var electron     = require('electron-prebuilt');
 var gulp         = require('gulp');
 
-
 gulp.task('run', function () {
     childProcess.spawn(electron, ['.'],
     { stdio: 'inherit' });
